798840faa2c7169ec1d7bde07122665d3e5a5392
Art Mills <art@hamcois.com>
2014-01-26 10:46:43 -0600

    Up to 8.2

--- lib/nice_bank.rb
+++ lib/nice_bank.rb
@@ -1,18 +1,14 @@
-#---
-# Excerpted from "The Cucumber Book",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/hwcuc for more book information.
-#---
 class Account
-  def deposit(amount)
+  def credit(amount)
     @balance = amount
   end
   
   def balance
     @balance
+  end
+
+  def debit(amount)
+    @balance -= amount
   end
 end
 
@@ -22,6 +18,7 @@ class Teller
   end
   
   def withdraw_from(account, amount)
+    account.debit(amount)
     @cash_slot.dispense(amount)
   end
 end
